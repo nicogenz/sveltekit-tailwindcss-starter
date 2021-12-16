bb17f5ccf33c3823b859ca2e956c364e0d9ab713
Nico Genz <mail@nicogenz.de>
2021-12-16 19:56:44 +0100

    add and configure tailwindcss

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="font-bold text-5xl text-blue-600">Welcome to SvelteKit + TailwindCSS</h1>
+<p>Visit <a href="https://kit.svelte.dev" class="underline text-blue-700 mt-12">kit.svelte.dev</a> to read the documentation</p>
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/index.html",
+    "./src/**/*.{svelte,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
